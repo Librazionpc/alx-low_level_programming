178943da1f348b65583e37edaf1b0ab23a08daf3
Librazionpc <nwajeizion393@gmail.com>
2023-07-13 21:59:45 +0100

    c programming language

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,9 +10,9 @@
 int main(void)
 {
 
-	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, text, 10);
+	write(2, text, 59);
 
 	return (1);
 
