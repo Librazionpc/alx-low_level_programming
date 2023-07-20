67d8ef95d3e085e32ecfd28f99be2a28e8f1c208
Librazionpc <nwajeizion393@gmail.com>
2023-07-20 13:02:35 +0100

    Funtiopn for the multiples of 3 and 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-void hy(void)
+
+/**
+ * multiples - Function that check for the multiples if 3 and
+ *
+ * Return: Always 0
+ */
+
+void multiples(void)
 {
 	int a, b;
+
 	for (a = 0; a < 1024; a++)
 	{
-		if ((a % 3) ==0 || (a % 5) == 0)
+		if ((a % 3) == 0 || (a % 5) == 0)
 		{
 			b += a;
 		}
 	}
-printf("%d\n",b);
+printf("%d\n", b);
 }
 
+/**
+ * main - the function that call the multiple function
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	hy();
+	return (0);
 }
 
