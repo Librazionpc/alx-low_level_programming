00708bba2eb8899d4abbfe2acc6f47e5e249d766
Librazionpc <nwajeizion393@gmail.com>
2023-07-20 17:42:40 +0100

    Function for Fizz Buzz game

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Function 
+ * main - Function of fizz-Buzz game
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 3) == 0 && (a % 5) == 0)
+		{
+			printf("Fizz Buzz ");
+		}
+		else if ((a % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((a % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+		printf("%d ", a);
+		}
+	}
+	return (0);
+}
