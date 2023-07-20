49a62d7c4727aa45ed9220ca0d560c9ef30190aa
Librazionpc <nwajeizion393@gmail.com>
2023-07-20 22:19:42 +0100

    Debbuging

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,20 +13,25 @@ int main(void)
 	{
 		if ((a % 3) == 0 && (a % 5) == 0)
 		{
-			printf("Fizz Buzz ");
+			printf("FizzBuzz");
 		}
 		else if ((a % 3) == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((a % 5) == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
 		printf("%d ", a);
 		}
+		if (a < 100)
+		{
+			putchar(' ');
+		}
 	}
+	putchar('\n');
 	return (0);
 }
