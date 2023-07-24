4b8d874dbd558e867d8138b9610b40577bd61b44
Librazionpc <nwajeizion393@gmail.com>
2023-07-24 14:44:25 +0100

    Suing pointers in c

---
+++ 0x05-pointers_arrays_strings/0-reset_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - A funtion that convert pointer to int as parameter
+ *
+ * @n: Fetch the argument from the main.c
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
