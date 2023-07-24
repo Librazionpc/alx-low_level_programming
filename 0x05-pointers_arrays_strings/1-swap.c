d5bb783aac49a794ca121eccc66b1f986a9d8b53
Librazionpc <nwajeizion393@gmail.com>
2023-07-24 16:43:17 +0100

    Using pointers in C

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,14 +3,13 @@
 /**
  * swap_int - Function that swaps the values of two integers.
  *
- * @n: fetches the argument from the main.c
+ * @a: fetches the argument from the main.c
  * @b: fetches the argument from the main.c
  *
  */
 void swap_int(int *a, int *b)
-{	
+{
 	int deep;
-	
 
 	deep = *a;
 	*a = *b;
