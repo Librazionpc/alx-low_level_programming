1206a9e6edccb0ad507843afb65c37325f12fbb0
Librazionpc <nwajeizion393@gmail.com>
2023-07-27 12:32:26 +0100

    Function that uses cipher logic to encode and decode message

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * rot13 - Function for cipher encoding letter
+ *
+ * @wrd: Funtion the message from C
+ *
+ * Return: The encoded message or unencoded
+ */
+char *rot13(char *wrd)
+{
+	int a = 0;
+
+	while (wrd[a] != '\0')
+	{
+		while ((wrd[a] >= 'a' && wrd[a] <= 'm') || (wrd[a] >= 'A' && wrd[a] >= 'M'))
+		{
+			if ((wrd[a] >= 'a' && wrd[a] <= 'm') || (wrd[a] <= 'A' && wrd[a] <= 'M'))
+				wrd[a] += 13;
+			else
+				wrd[a] -= 13;
+			a++;
+		}
+		a++;
+	}
+	return (wrd);
+}
+
+
