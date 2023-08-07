8939d3161248a3c9773e283b5c60fcd02dc2b43f
Librazionpc <nwajeizion393@gmail.com>
2023-08-07 23:40:59 +0100

    Function that concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,6 +26,9 @@ char *str_concat(char *s1, char *s2)
 	len_b = strlen(s2);
 
 	pt_string = (char *)malloc(sizeof(char) * (len_a + len_b + 1));
+	
+	if (pt_string == NULL)
+		return (NULL);
 	for (i = 0; i < len_a + len_b; i++)
 	{
 		pt_string[i] = *(s1 + i);
