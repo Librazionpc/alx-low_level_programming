3707ddd3df58e66128c41fc832c68bfec666d1cd
Librazionpc <nwajeizion393@gmail.com>
2023-08-08 02:11:45 +0100

    Free a dynamic created memory

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,4 +5,5 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *s);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int hieght);
+void free_grid(int **grid, int height);
 #endif
