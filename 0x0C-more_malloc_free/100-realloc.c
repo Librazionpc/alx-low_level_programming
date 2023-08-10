62d9fcf0fc2e5657995fb2786cfd0008ab4fce40
Librazionpc <nwajeizion393@gmail.com>
2023-08-10 18:52:26 +0100

    0x0C mandatory

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int a;
+	char *ptr_new;
+	char *save = (void* )ptr;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr_new = malloc(new_size);
+	if (ptr_new == NULL)
+		return (NULL);
+	for (a = 0; a < old_size; a++)
+		ptr_new[a] = save[a];
+	free(ptr);
+	return ((void *)ptr_new);
+}
