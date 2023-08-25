4dce26a7675a2f13f6b787674db6c7ea935b1d38
Librazionpc <nwajeizion393@gmail.com>
2023-08-25 14:51:17 +0100

    Dealing with Linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - Funtion that print from a linked list
+ *
+ * @h: Fetches the linked list
+ *
+ * Return: the size of the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *temp = h;
+	size_t count = 0;
+
+	while (temp != NULL)
+	{
+		if (temp->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", temp->len, temp->str);
+		count++;
+		temp = temp->next;
+
+	}
+	return (count);
+}
