0e0dfd1626a5b34c9ac2251569764193aa1cfd1d
Librazionpc <nwajeizion393@gmail.com>
2023-08-25 16:48:28 +0100

    Linked list with assembly lang

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,6 @@
 section .data
-    hello db 'Hello, Holberton!',0
-    hello_len equ $ - hello
+    hello db 'Hello, Holberton', 10 ; The string to print, followed by a newline (10 is ASCII for newline)
+    hello_len equ $ - hello         ; Calculate the length of the string
 
 section .text
     global _start
