518a143817a2d89e05ebb1a2b9b57cb0e00492af
Librazionpc <nwajeizion393@gmail.com>
2023-08-29 22:29:53 +0100

    debugging

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *save;
 	unsigned int i;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (NULL);
 
 	save = head;
