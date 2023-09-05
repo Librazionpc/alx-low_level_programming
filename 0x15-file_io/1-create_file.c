4eb015231c00d4a9509ea6f2098e5ff2508e361c
Librazionpc <nwajeizion393@gmail.com>
2023-09-05 04:00:50 +0100

    next

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,6 +5,14 @@
 #include <string.h>
 #include "main.h"
 
+/**
+ * create_file - Function that create a file
+ * @filename: The file name
+ * @text_content: The to written in the file
+ *
+ * Return: 1 on Success
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	const char *newFile = filename;
