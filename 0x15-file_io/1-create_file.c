76622254f930a4f011d91f6cadfab3a0197a82bf
Librazionpc <nwajeizion393@gmail.com>
2023-09-05 04:09:13 +0100

    Next

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,6 +25,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
+
 	if (text_content != NULL)
 	{
 		bytes_written = write(fd, text_content, strlen(text_content));
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,10 +7,10 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * append_text_to_file - Function that append text to end of a file
+ * @filename: The file name
+ * @text_content: The text to be appended
+ * Return: Always 0 (On Success)
  */
 
 int append_text_to_file(const char *filename, char *text_content)
@@ -25,5 +25,5 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (bytes_appended == -1)
 		return (-1);
 	close(fd);
-	return 0;
+	return (1);
 }
