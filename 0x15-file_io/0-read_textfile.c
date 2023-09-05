968a736792570ad26cb285dc9083c26589b83953
Librazionpc <nwajeizion393@gmail.com>
2023-09-05 03:53:00 +0100

    First push

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * read_textfile - Fuction that read text from a file
+ * @letters: The text to printed
+ * @filename: The file
+ * Return: The chars_printed int
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_descriptor;
+	ssize_t bytes_read = 0, bytes_written = 0;
+	size_t char_to_printed = 0, char_printed = 0;
+	char buffer[1024];
+
+	if (filename == NULL)
+		return (0);
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
+	while ((bytes_read = read(file_descriptor, buffer, (sizeof(buffer)) - 1)) > 0)
+	{
+		if (bytes_read == -1)
+			return (0);
+		char_to_printed = bytes_read;
+		if (letters < char_to_printed)
+			char_to_printed = letters;
+		bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+		if (bytes_written == -1
+		|| bytes_read > bytes_written
+		|| bytes_read > bytes_written)
+			return (0);
+		char_printed += char_to_printed;
+	}
+	close(file_descriptor);
+	return (char_printed);
+}
