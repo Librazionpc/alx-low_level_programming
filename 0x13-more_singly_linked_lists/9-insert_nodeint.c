bb1c4440d4592d1f2a65512e29f0c8576113939a
Librazionpc <nwajeizion393@gmail.com>
2023-09-28 01:23:34 +0100

    Dealing with idx conditions

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = save;
 		save = save->next;
 	}
-	if (i != idx)
+	if (save->next == NULL && i == idx)
 	{
 		free(newNode);
 		return (NULL);
