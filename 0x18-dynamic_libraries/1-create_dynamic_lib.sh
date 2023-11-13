ab9cdde59a83c759a48a49e8e53a858468e985d8
Librazionpc <nwajeizion393@gmail.com>
2023-11-13 16:55:56 +0100

    Dynamic link library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
-
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -shared -o liball.so *.o
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c && gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -shared -o liball.so *.o
