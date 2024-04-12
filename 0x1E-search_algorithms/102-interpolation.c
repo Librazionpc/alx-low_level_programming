4c89ffd8e5a92c3c8e618be3617449f5ab0e706e
Librazionpc <nwajeizion393@gmail.com>
2024-04-12 03:04:14 +0100

    Fixing Betty

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,9 +1,12 @@
 #include "search_algos.h"
 
 /**
+ * interpolation_search - Function that immplement interpolation search
+ * @array: The list of interger
+ * @size: The size of the array
+ * @value: The integer needed
  *
- *
- *
+ * Return: The position or -1 on failure
  */
 
 int interpolation_search(int *array, size_t size, int value)
@@ -11,13 +14,17 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t low = 0, high = size - 1, position, index_dif;
 	int value_dif;
 	double portion;
+
+	if (array == NULL)
+		return (-1);
+
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
 		index_dif = high - low;
-		value_dif = array[high] -array[low];
+		value_dif = array[high] - array[low];
 		portion = (double)(value - array[low]) / value_dif;
 		position = low + (index_dif * portion);
-		printf("Value checked array [%ld] = [%d]\n", position, array[position]);	
+		printf("Value checked array [%ld] = [%d]\n", position, array[position]);
 		if (array[position] == value)
 			return (position);
 
@@ -29,4 +36,3 @@ int interpolation_search(int *array, size_t size, int value)
 
 	return (-1);
 }
-
